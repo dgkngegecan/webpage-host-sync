08dcac2009431a37345552efee8fe74cbb9fc4bc
doğukan gegecan <gegebaski@gmail.com>
2025-12-13 04:59:59 +0300

    Update next.config.mjs

--- webpage/next.config.mjs
+++ webpage/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    serverActions: {
+        bodySizeLimit: '10mb',
+    },
     experimental: {
         serverActions: {
             bodySizeLimit: '10mb',
